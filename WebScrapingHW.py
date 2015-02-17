33f9fb8ff60eaf71e2094e7b519b242f66aca957
Mason Gamble <mgamble@ucla.edu>
2015-02-17 08:29:53 +0000

    Add Web scraping python file and demo html file (11 pages)

--- WebScrapingHW.py
+++ WebScrapingHW.py
@@ -13,7 +13,7 @@ main_pane = soup.find_all(id='main-content')
 #There are too many texts items all called <p>, with no other identifier to specify which one I want for names.
 #I don't think this is possible based on how the webpage is coded.
 
-my_file = open('ecoevo.txt')
+my_file = open('ecoevo.txt', 'a')
 
 for div in main_pane:
 	section_class = div.find('div', class_='section')
@@ -30,7 +30,7 @@ for div in main_pane:
 		
 # This only prints all the seminar dates, but worked
 
-my_file = open('ecoevo.txt')
+my_file = open('ecoevo.txt', 'a')
 
 for div in main_pane:
     section_class = div.find('div', class_='section')
